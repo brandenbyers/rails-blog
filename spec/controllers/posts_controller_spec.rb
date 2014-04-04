3093aeef82090212b03357d7452f48473451890d
brndnb <branden@fermup.com>
2014-04-04 13:54:53 -0500

    controller test for posts@index

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,4 +16,11 @@ describe PostsController do
     end
   end
   
+  describe 'GET #index' do
+    it "returns http success" do
+      get :index
+      response.should be_success
+    end
+  end
+  
 end
