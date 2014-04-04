ef529ddc66d4006010dd34e2d52e6881a578de6d
brndnb <branden@fermup.com>
2014-04-03 23:06:30 -0500

    fixed typo in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Blog::Application.routes.draw do
-  resources :posts
-
-  root 'welcome#index'
-
-  Blog::Application.routes.draw do
     resources :posts
     
     root 'welcome#index'
-  end
 end
