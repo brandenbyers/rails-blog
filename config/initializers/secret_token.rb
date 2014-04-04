d3aee8a056b2b3a2f62701042f86a125ebda8a93
brndnb <branden@fermup.com>
2014-04-03 20:54:26 -0500

    initial commit and first two steps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '8230fbfed92a8e7be8f4e9aec93342c38413c935b4e035accacbaec6c660a72ea918612bc48a40bf4b0a80872e7127babe97c8c70c62d80f536516aaf23aeb56'
