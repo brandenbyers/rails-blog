8a4c378f1c986f6ebb6bd74a247b117a9ebac9b2
brndnb <branden@fermup.com>
2014-05-21 14:18:38 -0500

    updated UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+  
+    def welcome_email(user)
+      @user = user
+      mail(to: @user.email, subject: 'Welcome')
+    end
 end
