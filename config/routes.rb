9c57fcc994ebb2163e49bb581306d925c5084bbd
brndnb <branden@fermup.com>
2014-06-20 13:23:55 -0500

    Add omniauth_callbacks to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Blog::Application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: "registrations", omniauth_callbacks: "omniauth_callbacks" }
   resources :posts do
     resources :comments
   end
