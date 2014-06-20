3f18f8ca7685bc22670e677679cdc7a1ed4b0885
brndnb <branden@fermup.com>
2014-06-20 13:37:36 -0500

    Add find_for_facebook_oath method to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ActiveRecord::Base
          
   has_many :posts
   has_many :comments
+
+  def self.find_for_facebook_oath(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+  end
 end
