eee5d01da0e24b7e6aa082039f46ae8486818121
brndnb <branden@fermup.com>
2014-06-21 05:46:51 -0500

    Change to proper chamber env calls and fixed typos

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,9 +230,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_KEY']
-  config.omniauth :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  config.omniauth :facebook, Chamber.env.FACEBOOK_KEY, Chamber.env.FACEBOOK_SECRET
+  config.omniauth :twitter, Chamber.env.TWITTER_KEY, Chamber.env.TWITTER_KEY
+  config.omniauth :linkedin, Chamber.env.LINKEDIN_KEY, Chamber.env.LINKEDIN_SECRET
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
