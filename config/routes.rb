1800bc68f804a842bbd01effe06467e67625292d
brndnb <branden@fermup.com>
2014-06-21 04:53:01 -0500

    Change call to Chamber for environment variables

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Blog::Application.routes.draw do
     resources :comments
   end
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
-
   root 'welcome#index'
 end
